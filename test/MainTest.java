17641a6e2747ad91418532c9e0d1b1b265606cf8
Denis Perdomo <joanp307@gmail.com>
2022-08-08 13:30:35 -0400

    Fixes and cleaner code

--- test/MainTest.java
+++ test/MainTest.java
@@ -4,20 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     final static int GOOD_DATA_LEN = 26;
-    final static int BAD_DATA_LEN = 25;
-
+    final static int BAD_DATA_LEN = 24;
+    final static String GOOD_DATA_FILENAME = "./test/goodInput.txt";
+    final static String BAD_DATA_FILENAME = "./test/badInput.txt";
 
     @DisplayName("Test valid events")
     @Test
     void testFillEventsGood() {
-        Main.fillEvents("./goodInput.txt");
+        Main.fillEvents(GOOD_DATA_FILENAME);
         assertEquals(GOOD_DATA_LEN, Main.events.size());
     }
 
     @DisplayName("Test invalid events")
     @Test
     void testFillEventsBad() {
-        Main.fillEvents("./badInput.txt");
+        Main.fillEvents(BAD_DATA_FILENAME);
         assertEquals(BAD_DATA_LEN, Main.events.size());
     }
 }
