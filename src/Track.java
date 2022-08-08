bfc528b6bf4aca195f2230061c827d73fe2d05c7
Denis Perdomo <joanp307@gmail.com>
2022-08-08 11:20:43 -0400

    get total times, add events

--- src/Track.java
+++ src/Track.java
@@ -11,11 +11,54 @@ public class Track
         morning = new ArrayList<>();
         evening = new ArrayList<>();
     }
-    //get morning time
-    //get evening time
-    //add morning return bool if event can be added
-    //add evening return bool if event can be added
 
+    /**
+     * 
+     * @return total duration of events in the morning so far, 0 if there are no events scheduled for the morning
+     */
+    public int getMorningTime()
+    {
+        return morning.parallelStream().reduce(0, (acc, next) -> acc + next.duration(), (t1, t2) -> t1 + t2);
+    }
+
+    /**
+     * 
+     * @return total duration of events in the evening so far, 0 if there are no events scheduled for the evening
+     */
+    public int getEveningTime()
+    {
+        return evening.parallelStream().reduce(0, (acc, next) -> acc + next.duration(), (t1, t2) -> t1 + t2);
+    }
+
+    /**
+     * Add event to the list of events scheduled for the morning
+     * @param event
+     * @return True if event was added, false if the event could not be added
+     */
+    public boolean addMorning(Event event)
+    {
+        if (getMorningTime() + event.duration() > MORNING_TIME) {
+            morning.add(event);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Add event to the list of events scheduled for the evening
+     * @param event
+     * @return True if event was added, false if the event could not be added
+     */
+    public boolean addEventing(Event event)
+    {
+        if (getEveningTime() + event.duration() > EVENING_TIME) {
+            evening.add(event);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     //todo
     @Override
