aff7ba508d694da4bc3e385f71d0b2790ef7b937
William Netz-Mowrer <wnetzmowrer@gmail.com>
2022-08-08 08:30:48 -0700

    file access

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main
 {
-    //ArrayList<Track>
-    //ArrayList<Event>
+    static ArrayList<Track> tracks;
+    static ArrayList<Event> events;
     public static void main(String[] args)
     {
         System.out.println("Hello World");
 
-
-        //fillEvents
+        fillEvents();
         //sort events
 
         //createTracks
@@ -15,9 +22,24 @@ public class Main
         //print solution
 
     }
-    public void fillEvents()
+    public static void fillEvents()
     {
-        //file access
+        List<String> evnts = new ArrayList<>();
+        try
+        {
+            evnts = Files.readAllLines(Paths.get("./src/input.txt"));
+        }
+        catch (IOException e)
+        {
+            System.out.println("no File");
+        }
+        events = new ArrayList<>(evnts.stream()
+                .filter(event -> event.split("^").length == 2)
+                .map(a->{
+                    String[] event = a.split("^");
+                    return new Event(event[0],Integer.valueOf(event[1]));
+                })
+                .collect(Collectors.toList()));
     }
     public void createTracks()
     {
